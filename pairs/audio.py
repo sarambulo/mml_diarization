4cc093b713a7d2542149137a2e596b679aad34eb
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2025-03-23 19:54:06 -0400

    updates based on convo w sukriti

--- pairs/audio.py
+++ pairs/audio.py
@@ -7,7 +7,7 @@ def get_speaker_status_by_frame(path):
     speakers = read_speaker_file(path)
 
 
-def divide_mfcc_into_frames(mfcc):
+def divide_mel_into_frames(mel):
     pass
 
 
@@ -19,34 +19,21 @@ def sample_speaker_not_speaking(speaker):
     pass
 
 
-def sample_positive_pair(speaker, status):
-    is_speaking = bool(status)
-    if is_speaking:
-        return sample_speaker_is_speaking(speaker)
-    else:
-        return sample_speaker_not_speaking(speaker)
-
-
-def sample_negative_pair(speaker, status):
-    is_speaking = bool(status)
-    if is_speaking:
-        return sample_speaker_not_speaking(speaker)
-    else:
-        return sample_speaker_is_speaking(speaker)
-
-
-def create_audio_pairs(chunks):
-    pairs = []
+def create_audio_pairs(chunks, output_dir):
     for chunk in chunks:
         speaker_status_list = get_speaker_status_by_frame(chunk["is_speaking"])
-        mfcc = read_spectrogram(chunk["spectrogram"])
-        mfcc_frames = divide_mfcc_into_frames(mfcc)
+        mel = read_spectrogram(chunk["spectrogram"])
+        mel_frames = divide_mel_into_frames(mel)
         for frame_id, frame_status in enumerate(speaker_status_list):
             for speaker_id, status in enumerate(frame_status):
-                anchor = mfcc_frames[frame_id]
-                pos_frame_id = sample_positive_pair(speaker_id, status)
-                neg_frame_id = sample_negative_pair(speaker_id, status)
-                if pos_frame_id != None and neg_frame_id != None:
-                    pos = mfcc_frames[pos_frame_id]
-                    neg = mfcc_frames[neg_frame_id]
-                    pairs.append((anchor, pos, neg))
+                is_speaking = bool(status)
+                if is_speaking:
+                    anchor = mel_frames[frame_id]
+                    pos_frame_id = sample_speaker_is_speaking(speaker_id)
+                    neg_frame_id = sample_speaker_not_speaking(speaker_id)
+                    if pos_frame_id != None and neg_frame_id != None:
+                        pos = mel_frames[pos_frame_id]
+                        neg = mel_frames[neg_frame_id]
+
+                        path = save_triplet(output_dir, (anchor, pos, neg))
+                        save_pair_to_csv(speaker_id, status, path)
--- pairs/main.py
+++ pairs/main.py
@@ -5,15 +5,11 @@ from utils import crawl_chunks_dir
 from config import *
 
 
-def create_pairs(
-    input_dir: str, output_dir: str, type: Literal["audio", "visual"]
-) -> None:
+def create_pairs(input_dir: str, output_dir: str) -> None:
     chunks = crawl_chunks_dir(input_dir)
-    if type == "audio":
-        create_audio_pairs(chunks, output_dir)
-    else:
-        create_video_pairs(chunks, output_dir)
+    create_audio_pairs(chunks, output_dir)
+    create_video_pairs(chunks, output_dir)
 
 
 if __name__ == "__main__":
-    create_pairs(INPUT_DIR, OUTPUT_DIR, "audio")
+    create_pairs(INPUT_DIR, OUTPUT_DIR)
