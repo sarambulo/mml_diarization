1a15be04b4c17aac47a634f13fcec9d77a615304
sukritirawal1 <rawalsukriti@gmail.com>
2025-03-24 00:49:28 -0400

    added combined_pairs function + collecting/saving info in CSV

--- pairs/video.py
+++ pairs/video.py
@@ -1,4 +1,5 @@
 from pairs.utils import *
+from config import *
 from collections import defaultdict
 import numpy as np
 import pandas as pd
@@ -6,8 +7,8 @@ import random
 import os
 
 def create_lookups(df):
-    speaker_frames = defaultdict(list)  #speaker -> list of (chunk_id, frame_id) tuples
-    chunk_speakers = defaultdict(list)  #chunk id -> list of (speaker_id, frame_id) tuples  
+    speaker_frames = defaultdict(list)  #speaker -> list of (chunk_id, frame_id, is_speaking) tuples
+    chunk_speakers = defaultdict(list)  #chunk id -> list of (speaker_id, frame_id, is_speaking) tuples  
     
     for _, row in df.iterrows():
         spk = row['speaker_id']
@@ -15,7 +16,7 @@ def create_lookups(df):
         fr = row['frame_id']
         is_speaking = row['is_speaking']
         speaker_frames[spk].append((ch, fr, is_speaking))
-        chunk_speakers[ch].append((spk, fr))
+        chunk_speakers[ch].append((spk, fr, is_speaking))
     
     return speaker_frames, chunk_speakers
 
@@ -32,7 +33,7 @@ def get_positive_pair(current_chunk, current_frame, anchor_frames):
         
 def get_visual_negative_pair(anchor_speaker_id, current_chunk, chunk_speakers):
     speakers_by_chunk = chunk_speakers[current_chunk] #list of (speaker, frame) tuples
-    other_speaker_frames = [(spk, fr) for spk, fr in speakers_by_chunk 
+    other_speaker_frames = [(spk, fr) for spk, fr, _ in speakers_by_chunk 
                                if spk != anchor_speaker_id]
     if other_speaker_frames: #check if found in same chunk
         neg_face_id, neg_frame_id =  random.choice(other_speaker_frames)
@@ -48,8 +49,26 @@ def get_visual_negative_pair(anchor_speaker_id, current_chunk, chunk_speakers):
                 return chunk_id, neg_face_id, neg_frame_id
     return None
 
-def get_combined_negative_pair(anchor_speaker_id, current_chunk, chunk_speakers):
-    pass
+def get_combined_negative_pair(anchor_speaker_id, current_chunk, chunk_speakers, sample_diff_speaker_prob):
+    is_speaking_val = 1 if np.random.uniform(0,1) <= sample_diff_speaker_prob else 0
+    speakers_by_chunk = chunk_speakers[current_chunk]
+    
+    other_speaker_frames = [(spk, fr) for spk, fr, is_speaking in speakers_by_chunk 
+                                if spk != anchor_speaker_id and is_speaking == is_speaking_val]
+    if other_speaker_frames: #check if found in same chunk
+        neg_face_id, neg_frame_id =  random.choice(other_speaker_frames)
+        return current_chunk, neg_face_id, neg_frame_id
+    else:
+        remaining_chunk_ids = [ch for ch in chunk_speakers.keys() if ch != current_chunk]
+        for chunk_id in remaining_chunk_ids:
+            speakers_by_chunk = chunk_speakers[chunk_id]
+            other_speaker_frames = [(spk, fr, is_speaking) for spk, fr,is_speaking in speakers_by_chunk 
+                            if spk != anchor_speaker_id and is_speaking == is_speaking_val]
+            if other_speaker_frames:
+                neg_face_id, neg_frame_id = random.choice(other_speaker_frames)
+                return chunk_id, neg_face_id, neg_frame_id
+    return None
+
 
 #INPUT: is_speaking.csv
 #output: pairs.csv with 7 new columns:  PosChunkID, PosFrameID, NegChunkID, NegFrameID, NegSpeakerID, Video_Flag, Audio_Flag
@@ -57,7 +76,7 @@ def create_pairs(input_file_path):
     df = pd.read_csv(input_file_path, 
                 names=['chunk_id', 'speaker_id', 'is_speaking', 'timestamp', 'frame_id'])
     
-    speaker_frames, chunk_speakers = create_lookups(df) #add is_speaking to create_lookups() function
+    speaker_frames, chunk_speakers = create_lookups(df)
     
     for anchor_speaker in speaker_frames: #iterate through speakers
         anchor_frames = speaker_frames[anchor_speaker] #get frames for current speaker
@@ -65,15 +84,36 @@ def create_pairs(input_file_path):
         anchor_speaking_frames = [(ch, fr, is_speaking) for (ch, fr, is_speaking) in anchor_frames if is_speaking == 1]
         anchor_non_speaking_frames = [(ch, fr,is_speaking) for (ch, fr, is_speaking) in anchor_frames if is_speaking == 0]
         
-        pair_info = {}
+        pair_info = {'chunk_id': [],
+                     'speaker_id': [],
+                     'is_speaking': [],
+                     'frame_id':[],
+                     'pos_chunk_id':[],
+                     'pos_frame_id':[],
+                     'neg_chunk_id':[],
+                     'neg_speaker_id':[],
+                     'neg_frame_id':[],
+                     'video_flag':[],
+                     'audio_flag':[]}
+                     
         skipped_frames = [] #collect frames which are speaking but combined pair is not found
         
         for current_frame, current_chunk, _ in anchor_speaking_frames:
             pos_chunk, pos_frame = get_positive_pair(current_chunk, current_frame, anchor_speaking_frames) #get positive pair from speaking_frames only
-            neg_chunk, neg_speaker, neg_frame = get_combined_negative_pair() #to be implemented - get negative pair for different face, speaking vs not speaking depends on probability
+            neg_chunk, neg_speaker, neg_frame = get_combined_negative_pair(anchor_speaker, current_chunk, chunk_speakers, AUDIO_SAMPLE_DIFFERENT_SPEAKER_PROB) #to be implemented - get negative pair for different face, speaking vs not speaking depends on probability
             if pos_chunk and neg_chunk:
-                video_flag, audio_flag = True, True
-                #append to pair info
+                pair_info['chunk_id'].append(current_chunk)
+                pair_info['speaker_id'].append(anchor_speaker)
+                pair_info['is_speaking'].append(1) #always speaking
+                pair_info['frame_id'].append(current_frame)
+                pair_info['pos_chunk_id'].append(pos_chunk)
+                pair_info['pos_frame_id'].append(pos_frame)
+                pair_info['neg_chunk_id'].append(neg_chunk)
+                pair_info['neg_speaker_id'].append(neg_speaker)
+                pair_info['neg_frame_id'].append(neg_frame)
+                pair_info['video_flag'].append(1) #works for both both video and audio pairs
+                pair_info['audio_flag'].append(1)
+                
             else:
                 skipped_frames.append((current_frame, current_chunk, 1))
                 
@@ -81,14 +121,25 @@ def create_pairs(input_file_path):
             pos_chunk, pos_frame = get_positive_pair(current_chunk, current_frame, anchor_frames) #get positive pair from any anchor frame
             neg_chunk, neg_speaker, neg_frame = get_visual_negative_pair(anchor_speaker, current_chunk, chunk_speakers) #get negative pair without speaking restriction
             if pos_chunk and neg_chunk:
-                video_flag, audio_flag = True, False
-            #append to pair info
-        
+                pair_info['chunk_id'].append(current_chunk)
+                pair_info['speaker_id'].append(anchor_speaker)
+                pair_info['is_speaking'].append(is_speaking) #is_speaking could be different
+                pair_info['frame_id'].append(current_frame)
+                pair_info['pos_chunk_id'].append(pos_chunk)
+                pair_info['pos_frame_id'].append(pos_frame)
+                pair_info['neg_chunk_id'].append(neg_chunk)
+                pair_info['neg_speaker_id'].append(neg_speaker)
+                pair_info['neg_frame_id'].append(neg_frame)
+                pair_info['video_flag'].append(1) 
+                pair_info['audio_flag'].append(0) #works for visual pairs only
+                
+        save_pair_info(pair_info)
         return pair_info
 
 def save_pair_info(pair_info_dict, output_file_path):
-    #save pair_info_dict to pairs.csv
-    pass
+    pairs_df = pd.DataFrame(pair_info_dict)
+    pairs_df.to_csv(output_file_path, index=False)
+    
 
 
 def main():
