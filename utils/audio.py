c10062aa03f9fc93ec52764fe14f9dfffee31002
Kanupriya <AnuranjanAnand@macbook-air-109.wifi.local.cmu.edu>
2025-03-26 20:09:56 -0400

    downsampling updated

--- utils/audio.py
+++ utils/audio.py
@@ -2,36 +2,6 @@ from typing import Dict, Tuple, List
 import torch
 import numpy as np
 import librosa
-# def read_audio(video_path: str, seconds: float = 3) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-#    """
-#    Reads a video file and returns video data, audio data, and timestamps.
-
-#    :param video_path: Path to the video file.
-#    :param seconds: Duration of the video to read in seconds.
-
-#    :return: A generator that yields a tuple containing:
-
-#       - video_data (torch.Tensor): Shape (Frames, C, H, W)
-#       - audio_data (torch.Tensor): Shape (Frames, SamplingRate, C)
-#       - timestamps (torch.Tensor): Shape (Frames,)
-#       - frame_ids  (torch.Tensor): Shape (Frames,)
-#    """
-#    return
-
-# def downsample_audio(video_frames: torch.Tensor, timestamps: torch.Tensor, frame_ids: torch.Tensor, factor: int = 5) -> Tuple[torch.Tensor, torch.Tensor]:
-#    """
-#    Downsamples a video and returns the remaining frames, timestamps and frame IDs.
-#    The number of remaining frames is `ceil(Frames / factor)`
-   
-#    :param video_frames: Shape (Frames, FPS, C, H, W)
-#    :param timestamps: Shape (Frames,
-
-#   :return: A tuple containing:
-#       - video_data (torch.Tensor): Shape (ceil(Frames / factor), C, H, W)
-#       - timestamps (torch.Tensor): Shape (ceil(Frames / factor),)
-#       - frame_ids  (torch.Tensor): Shape (ceil(Frames / factor),)
-#    """
-#    return
 
 def flatten_audio(audio_data: np.ndarray) -> np.ndarray:
    
@@ -59,39 +29,48 @@ def flatten_audio(audio_data: np.ndarray) -> np.ndarray:
 
 def transform_audio(
     audio_data: np.ndarray,
-    output_type: str,
     sr: int,
     n_bands: int,
+    target_tf: int,
     n_fft=1024,
-    hop_length=512,
+    hop_length=512
+
 ) -> np.ndarray:
     """
     Transforms audio into a log-mel spectrogram using librosa.
     
     Parameters:
         audio_data (np.ndarray): Mono audio data (1D array).
-        output_type (str): mfcc or melspectrogram
         sr (int): Sampling rate of the audio. Approx 44k.
         n_bands (int): Number of output bands to generate ~30
         fmax (int): Maximum frequency when converting to Mel scale. Typically sr/2.
 
     Returns:
-        np.ndarray: The log-mel spectrogram of shape (n_mels, time_frames= approx 10ms).
+        np.ndarray: The log-mel spectrogram of shape (n_mels, time_frames per segment, segments).
     """
+
     audio_data = audio_data.transpose(1, 0) # Put the channel first
-    if output_type == 'mfcc':
-        audio_data = audio_data.detach().cpu().numpy()
-        if audio_data.ndim > 1:
-            audio_data = np.mean(audio_data, axis=0)
-        mfcc = librosa.feature.mfcc(
-            y=audio_data, sr=sr, n_mfcc=n_bands, n_fft=n_fft, hop_length=hop_length
-        )
-        return mfcc
-    elif output_type == 'melspectrogram':
-        mel_spec = librosa.feature.melspectrogram(
+    audio_data = audio_data.detach().cpu().numpy()
+    if audio_data.ndim > 1:
+        audio_data = np.mean(audio_data, axis=0)
+    mfcc = librosa.feature.mfcc(y=audio_data, sr=sr, n_mfcc=n_bands, n_fft=n_fft, hop_length=hop_length)
+        
+
+    mel_spec = librosa.feature.melspectrogram(
             y=audio_data, sr=sr, n_mels=n_bands, n_fft=n_fft, hop_length=hop_length
         )
-        mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
-        return mel_spec_db
-    else:
-        raise ValueError(f'Invalid output type')
+    mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
+
+    if mfcc.shape[1]<target_tf:
+        pad_width=target_tf-mfcc.shape[1]
+        mfcc=np.pad(mfcc, ((0, 0), (0, pad_width)), mode='constant')
+        mel_spec_db=np.pad(mel_spec_db, ((0, 0), (0, pad_width)), mode='constant')
+    elif mfcc.shape[1]>target_tf:
+        mfcc=mfcc[:,:target_tf]
+        mel_spec_db= mel_spec_db[:,:target_tf]
+    mel_spec_db=mel_spec_db.reshape(n_bands, int(target_tf/20), 20)
+    mfcc=mfcc.reshape(n_bands, int(target_tf/20), 20)
+    return mel_spec_db, mfcc
+    
+       # (30,440) pad/truncate
+   # reshape (30, 22, 20)
