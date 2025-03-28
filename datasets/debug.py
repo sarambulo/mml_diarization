f721ca04ce076f5fa0025762b519beef6417b15e
Kanupriya <AnuranjanAnand@MacBook-Air-109.local>
2025-03-27 23:02:12 -0400

    relative paths

--- datasets/debug.py
+++ datasets/debug.py
@@ -34,8 +34,8 @@ def debug_dataset(dataset, num_samples: int = 1):
             print(f"Sample: {type(sample)} - {sample}")
 
 
-data_path = "/Users/AnuranjanAnand/Desktop/MML/mml_diarization/preprocessed"  # base directory
-partition_path = "/Users/AnuranjanAnand/Desktop/MML/mml_diarization/data_sample/few_train.rttm"  # your rttm partition file
+data_path = "preprocessed"  # base directory
+partition_path = "data_sample/few_train.rttm"  # your rttm partition file
 
 dataset = MSDWildChunks(data_path=data_path, partition_path=partition_path, subset=1.0)
 debug_dataset(dataset, num_samples=3)
