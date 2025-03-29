e2ea520dd9e098a335d207d4bc4ab1396d3a6119
Kanupriya <AnuranjanAnand@MacBook-Air-109.local>
2025-03-29 17:36:51 -0400

    still buggy

---
+++ models/visual_train.py
@@ -0,0 +1,157 @@
+from torch.utils.data import DataLoader
+import sys
+sys.path.insert(0, 'datasets')
+from MSDWild import MSDWildChunks
+import numpy as np
+from pathlib import Path
+import random
+import torch
+import pandas as pd
+import torch.nn.functional as F
+from .VisualOnly import VisualOnlyModel
+from sklearn.cluster import AgglomerativeClustering
+from losses.DiarizationLoss import DiarizationLoss
+from tqdm import tqdm
+
+CHECKPOINT_PATH = 'checkpoints'
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+@torch.no_grad()
+def get_metrics(logits, labels):
+    pred_labels = torch.argmax(logits, dim=-1)
+    n = labels.shape[0]
+    n_correct = pred_labels[pred_labels==labels].shape[0]
+    accuracy = n_correct / n
+    return accuracy
+
+def save_model(model, metrics, epoch, path):
+    checkpoint = {
+        'epoch': epoch + 1,
+        'model_state_dict': model.state_dict(),
+        'metrics': metrics,
+    }
+    torch.save(checkpoint, path)
+
+def train_epoch(model, dataloader, optimizer, criterion):
+    model.train()
+
+    # Progress Bar
+    batch_bar = tqdm(total=len(dataloader), dynamic_ncols=True, leave=False, position=0, desc='Train', ncols=5)
+    avg_loss = 0
+    avg_accuracy = 0
+    num_batches = len(dataloader)
+    for i, batch in enumerate(dataloader):
+        visual_batch=batch[0]
+        labels=batch[2]
+        print(visual_batch.shape)
+        print(len(labels))
+        batch_size, anchors, positive_pairs, negative_pairs = visual_batch
+        optimizer.zero_grad() # Zero gradients
+
+        # Join all inputs
+        batch_size = labels.shape[0]
+        features = list(zip(anchors, positive_pairs, negative_pairs))
+        print((features[0]))
+        # feature: [(batch_size, ...), (batch_size, ...), (batch_size, ...)]
+        # send to cuda
+        for index, feature in enumerate(features):
+            features[index] = torch.concat(feature, dim=0).to(DEVICE)
+            # feature: (batch_size * 3, ...)
+        labels = labels.to(DEVICE)
+
+        # forward
+        with torch.amp.autocast(DEVICE):  # This implements mixed precision. Thats it!
+            embeddings, logits = model(features)
+            anchors        = embeddings[            :   batch_size]
+            positive_pairs = embeddings[  batch_size: 2*batch_size]
+            negative_pairs = embeddings[2*batch_size: 3*batch_size]
+            logits         = logits[:batch_size]
+            # Use the type of output depending on the loss function you want to use
+
+            loss = criterion(anchors, positive_pairs, negative_pairs, logits, labels)
+
+        loss.backward() # This is a replacement for loss.backward()
+        optimizer.step() # This is a replacement for optimizer.step()
+
+        accuracy = get_metrics(logits, labels)
+        avg_loss = (avg_loss * i + loss.item()) / (i + 1)
+        avg_accuracy = (avg_accuracy * i + accuracy) / (i + 1)
+        # tqdm lets you add some details so you can monitor training as you train.
+        batch_bar.set_postfix(
+            acc   = "{:.04%} ({:.04%})".format(accuracy, avg_accuracy),
+            loss  = "{:.04f} ({:.04f})".format(loss.item(), avg_loss),
+            lr    = "{:.06f}".format(float(optimizer.param_groups[0]['lr']))
+        )
+
+        batch_bar.update() # Update tqdm bar
+
+    batch_bar.close()
+
+    return accuracy, loss
+
+
+data_path = "preprocessed"
+partition_path_train = "data_sample/few_train.rttm"
+partition_path_val = "data_sample/few_train.rttm"
+train_dataset = MSDWildChunks(data_path=data_path, partition_path=partition_path_train, subset=1.0)
+val_dataset= MSDWildChunks(data_path=data_path, partition_path=partition_path_val, subset=1.0)
+
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=train_dataset.build_batch)
+val_loader=DataLoader(val_dataset, batch_size=4, shuffle=False, collate_fn=val_dataset.build_batch)
+print(train_dataset)
+print(f"Dataset size: {len(train_dataset)}")
+
+for batch_idx, (visual_data, audio_data, is_speaking) in enumerate(train_loader):
+    print(f"\n--- Batch {batch_idx} ---")
+    print(f"visual_data shape: {visual_data.shape}")
+    print(f"audio_data shape:  {audio_data.shape}")
+    print(f"is_speaking shape: {is_speaking.shape}")
+    
+    # If you just want to check the first batch, break after printing:
+    break
+
+model = VisualOnlyModel(embedding_dims=512, num_classes=2) 
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+criterion = DiarizationLoss(0.5, 0.5)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, 'min', factor=0.1, patience=2, threshold=0.01
+    )
+start_epoch = 0
+final_epoch = 10
+metrics = {}
+best_valid_acc = 0
+for epoch in range(start_epoch, final_epoch):
+        print("\nEpoch {}/{}".format(epoch+1, final_epoch))
+        # train
+        curr_lr = float(scheduler.get_last_lr()[0])
+        metrics.update({'lr': curr_lr})
+        train_acc, train_loss = train_epoch(model, train_loader, optimizer, criterion)
+        print("\nEpoch {}/{}: \nTrain Cls. Acc {:.04f}%\t Train Cls. Loss {:.04f}\t Learning Rate {:.04f}".format(epoch + 1, final_epoch, train_acc, train_loss, curr_lr))
+        metrics.update({
+            'train_cls_acc': train_acc,
+            'train_loss': train_loss,
+        })
+        # validation
+        valid_acc, valid_loss = 0, 0 # TODO: evaluate_model(model, val_dataloader)
+        print("Val Cls. Acc {:.04f}%\t Val Cls. Loss {:.04f}".format(valid_acc, valid_loss))
+        metrics.update({
+            'valid_cls_acc': valid_acc,
+            'valid_loss': valid_loss,
+        })
+
+        # save best model
+        if valid_acc >= best_valid_acc:
+            best_valid_acc = valid_acc
+            model_path = Path(CHECKPOINT_PATH, f'best_visual.pth')
+            save_model(model, metrics, epoch, model_path)
+            print("Saved best model")
+
+        
+        # You may want to call some schedulers inside the train function. What are these?
+        if scheduler is not None:
+            scheduler.step(valid_loss)
+
+    # save last model
+model_path = Path(CHECKPOINT_PATH, f'last_visual.pth')
+save_model(model, metrics, epoch, model_path)
+print("Saved best model")
